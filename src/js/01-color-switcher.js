09fca06aeaa0a91df5eb85951fed870d933ee345
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-02-16 14:54:28 +0200

    4 коммит

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -19,31 +19,3 @@ const bodyColorStop = (event) => {
 }
 btnStopEl.addEventListener('click', bodyColorStop);
 btnStartEl.addEventListener('click', bodyColorStarInterval);
-
-
-
-
-
-
-const lalka = (event) => {
-  event.preventDefault();
-  const delayEl = Number(inputDelayEl.value);
-  let stepEl = Number(inputStepEl.value);
-  const amount = Number(inputAmountEl.value);
-  let mmm = 0;
-  intervalId = setInterval(() => {
-    mmm ++;
-    kokoko = stepEl;
-    if(mmm == amount){
-      clearInterval(intervalId);
-    }
-    createPromise( mmm ,stepEl)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-  }, stepEl) 
-}
-  
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,39 +4,48 @@ const inputAmountEl = document.querySelector("input[name='amount']");
 const btnEl = document.querySelector('button');
 const formEl = document.querySelector('.form');
 let intervalId = null;
-function createPromise(position, delay) { 
-  
-    return new Promise((resolve, reject) => {    
-      setTimeout(() => {
+function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {     
         const shouldResolve = Math.random() > 0.3;      
         if (shouldResolve) {
           resolve({position, delay})
         } 
-          reject({position, delay})
-      }, delay)
-                 
+          reject({position, delay})      
 })     
-
 }
+
 const lalka = (event) => {
   event.preventDefault();
   const delayEl = Number(inputDelayEl.value);
-  let stepEl = Number(inputStepEl.value);
+  const stepEl = Number(inputStepEl.value);
   const amount = Number(inputAmountEl.value);
-for (let index = 1; index <= amount; index += 1) {
-
-    createPromise(  index, stepEl )
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-
+  let mmm = 1;
+  setTimeout(() => {
+    createPromise( 1 ,delayEl)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  },delayEl)
+  setTimeout(() => {
+    intervalId = setInterval(() => {
+      mmm ++;
+      if(mmm == amount){
+        clearInterval(intervalId);
+      }
+      createPromise( mmm ,stepEl)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      console.log(stepEl)
+    }, stepEl) 
+  }, delayEl) 
 }
-
-}
-
 formEl.addEventListener('submit', lalka);
 
 
