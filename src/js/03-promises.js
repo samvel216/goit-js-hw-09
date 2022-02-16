4be76ae504bf716947903138af214e09a53ba5b1
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-02-16 11:32:16 +0200

    2 коммит

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,43 +7,36 @@ let intervalId = null;
 function createPromise(position, delay) { 
   
     return new Promise((resolve, reject) => {    
+      setTimeout(() => {
         const shouldResolve = Math.random() > 0.3;      
         if (shouldResolve) {
           resolve({position, delay})
         } 
-          reject({position, delay})            
+          reject({position, delay})
+      }, delay)
+                 
 })     
 
 }
-
 const lalka = (event) => {
   event.preventDefault();
   const delayEl = Number(inputDelayEl.value);
   let stepEl = Number(inputStepEl.value);
   const amount = Number(inputAmountEl.value);
-  let mmm = 0;
- 
-  intervalId = setInterval(() => {
-    mmm ++;
-    kokoko = stepEl;
-    if(mmm == amount){
-      clearInterval(intervalId);
-    }
-    setTimeout(() => {
-    createPromise( mmm ,stepEl)
+for (let index = 1; index <= amount; index += 1) {
+
+    createPromise(  index, stepEl )
     .then(({ position, delay }) => {
       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     });
-  },delayEl)
-  }, stepEl)
- 
-    
- 
-  
+
 }
+
+}
+
 formEl.addEventListener('submit', lalka);
 
 
